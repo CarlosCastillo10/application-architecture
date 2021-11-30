325fe933ee694500029be70785cc263a0f19b0c1
CarlosCastillo10 <carloscastillo090916@gmail.com>
2021-11-30 15:57:17 -0500

    :sparkles: Adding model layer

---
+++ python-flask/apiRestful/model/student.py
@@ -0,0 +1,25 @@
+class Student:
+    def __init__(self, name, lastname, age):
+        self.name = name
+        self.lastname = lastname
+        self.age = age
+
+    # Setter methods
+    def set_name(self, name):
+        self.name = name
+
+    def set_lastname(self, lastname):
+        self.lastname = lastname
+
+    def set_age(self, age):
+        self.age = age
+
+    # Getter methods
+    def get_name(self):
+        return self.name
+
+    def get_lastname(self):
+        return self.lastname
+
+    def get_age(self):
+        return self.age
